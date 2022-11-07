b240cf53e2b936bc85c21af33439b8f870f01894
developer-kawsar <kawsar.qtec@gmail.com>
2022-11-07 18:49:33 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn">Button</button>
+    </div>
+  );
+}
+
+export default App;
